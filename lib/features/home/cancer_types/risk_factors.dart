fe31ea894e63d51bc4d679fd8e5d030163ed248d
Ahmed Qala3awyi <a7medAlqal3awyi>
2024-02-16 21:29:50 +0200

    Added cancer fact screen

---
+++ lib/features/home/cancer_types/risk_factors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:skin_cancer_app/features/home/widget/cancer_type_widget.dart';
+
+class RiskFactorsScreen extends StatelessWidget {
+  const RiskFactorsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
